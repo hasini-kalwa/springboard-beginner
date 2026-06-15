60da3de2772aa6efb6e4fd8ab1baf2aafe1dc200
hasini-kalwa <kalwa.hasini@gmail.com>
2026-06-15 23:02:53 +0530

    integrated html and css along with springboot setup

---
+++ src/main/java/com/hasinik/springbootbeg/SpringbootbegApplication.java
@@ -0,0 +1,13 @@
+package com.hasinik.springbootbeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasinik/springbootbeg/SpringbootbegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasinik.springbootbeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
